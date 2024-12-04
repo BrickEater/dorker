b51e573fbc157c0b0a8704d608f93a64e9ddc85d
BrickEater <codyratcliffep@gmail.com>
2024-12-03 19:57:04 -0700

    almost working, need to change how the how the subdomain and excluded sets are initiated

--- 002.py
+++ 002.py
@@ -1,21 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from urllib.parse import urlparse
+import re
 
+# Init----------------------------------------------------------------
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(2)
 
-driver.get("https://www.google.com/search?q=selenium+prettyprint+source")
-anchors = driver.find_elements(By.TAG_NAME, "a")
-for anchor in anchors:
-    url = anchor.get_attribute("href")
-    if url:
-        parsed_url = urlparse(url)
-        if "google" not in parsed_url.netloc:
-            root_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-            print(root_url)
+# Variables-----------------------------------------------------------
+subdomains = set()
+excluded_subdomains = set()
+primary_domain = "site%3Amozilla.com"
+subdomains_found = True
+token = "+-"
+exclude = ""
+query = f"https://www.google.com/search?q={primary_domain}{exclude}"
+
+
+# Main Loop-----------------------------------------------------------
+
+
+while subdomains_found:
+    print(f"Query: {query}")
+    driver.get(query)
+    cite_tags = driver.find_elements(By.TAG_NAME, "cite")
+
+    for cite in cite_tags:
+        cite_content = cite.text
+        parsed_cite = urlparse(cite_content)
+        cite_scheme = re.sub(r"[^ -~]", "", parsed_cite.scheme)
+        cite_netloc = (re.sub(r"[^ -~]", "", parsed_cite.netloc)).split(" ")[0]
+
+        if cite_scheme and cite_netloc:
+            joined_url = f"{cite_scheme}://{cite_netloc}"
+            parts = cite_netloc.split(".")
+            if len(parts) > 2:
+                excluded_subdomains.add(parts[0])
+            subdomains.add(joined_url)
+
+        else:
+            # subdomains_found = False
+            break
+
+    for domain in subdomains:
+        print(domain)
+
+    for domain in excluded_subdomains:
+        print(domain)
+
+    if excluded_subdomains:
+        exclude = token + token.join(excluded_subdomains)
 
 
 input("Press any key to exit...")
